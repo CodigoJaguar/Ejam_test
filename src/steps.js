677fe5ac8920fc41c35088fffc6de0ee833f4459
CodigoJaguar <israel.san_mendoza@hotmail.com>
2023-04-05 10:13:25 -0600

    instance2

--- src/steps.js
+++ src/steps.js
@@ -6,23 +6,75 @@ const checkbox_paypal = document.getElementById('paypal_check');
 const checkbox_cc = document.getElementById('cc_check');
 const step_3 = document.getElementById('step_3');
 const step_4 = document.getElementById('step_4');
+const step_4_addr = document.getElementById('step_4_shipping')
+const step_5 = document.getElementById('step_5')
 const product_1 = document.getElementById('check_1');
 const product_2 = document.getElementById('check_2');
 const product_3 = document.getElementById('check_3');
 const product_4 = document.getElementById('check_4');
 const product_5 = document.getElementById('check_5');
 const products  = [product_1,product_2,product_3,product_4,product_5];
+const checkbox_shipping_addr = document.getElementById('shipping_addr');
+const checkbox_diff_addr     = document.getElementById('diff_addr');
+let Country_Selector = document.getElementById('country_selector')
 
 
 step_3.style.display = 'none';
 step_4.style.display = 'none';
+step_4_addr.style.display = 'none'
+step_5.style.display = 'none';
+
+let Countries = [];
+
+fetch('https://restcountries.com/v3.1/all').then(function (response) {
+	console.log('success!');
+    return response.json();
+}).then(function (data) {
+    try {
+
+        for (const iterator of data) {
+            const obj = {};
+            obj.name = iterator.name?.common;
+            obj.ext  = iterator.idd?.root;
+            if (!obj.name || !obj.ext) continue;
+            
+            try {
+                const subfix = obj.ext + iterator.idd?.suffixes[0];
+                obj.ext = subfix
+            } catch (error) {
+                console.log('errorconcat')
+                obj.ext.concat('-')
+            }
+            Countries.push(obj)
+        }
+        
+    } catch (error) {   
+        console.log(error);
+    }
+}).catch(function (err) {
+	console.warn('Something went wrong.', err);
+});
+
+Country_Selector.addEventListener('click', (e)=>{
+    
+        Countries.map(country=>{
+            const option = document.createElement('option');
+            option.innerHTML = country.name;
+            Country_Selector.appendChild(option);
+        })
+      
+});
+
+
+
+
 
 
 checkbox_paypal.addEventListener('change', (e)=>{
     e.preventDefault();
-    checkbox_cc.checked = false
-    console.log('Checkbox paypal change');
+    checkbox_cc.checked = false;
     step_3.style.display = 'block';
+    step_5.style.display = 'block';
     
     if (checkbox_paypal.checked) {
         step_4.style.display = 'block';
@@ -32,11 +84,12 @@ checkbox_paypal.addEventListener('change', (e)=>{
 
 checkbox_cc.addEventListener('change', (e)=>{
     e.preventDefault();
-    checkbox_paypal.checked = false
-    console.log('Checkbox cc change');
+    checkbox_paypal.checked = false;
+
 
     step_3.style.display = 'block';
     step_4.style.display = 'none';
+    step_4_addr.style.display  = 'block'; 
 });
 
 
@@ -50,6 +103,15 @@ for (let index = 0; index < products.length  ; index++) {
         product_4.checked = false;
         product_5.checked = false;
         element.checked   = true;
-        console.log('product check')
+        
     })
 }
+
+checkbox_shipping_addr.addEventListener('change',(e)=>{
+    checkbox_diff_addr.checked = false;
+
+});
+
+checkbox_diff_addr.addEventListener('change',(e)=>{
+    checkbox_shipping_addr.checked = false;
+})
